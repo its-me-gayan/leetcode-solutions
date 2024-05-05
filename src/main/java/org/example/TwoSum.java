331460a3258b36ea0faccacd5dbe9096527292d3
gayan <reachme.gayan@gmail.com>
2024-05-05 22:57:15 +0800

    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.

---
+++ src/main/java/org/example/TwoSum.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Author: Gayan Sanjeewa
+ * User: gayan
+ * Date: 5/5/24
+ * Time: 10:46 PM
+ */
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] num = new int[]{3,2,4};
+        System.out.println(Arrays.toString(twoSumMethod1(num, 6)));
+        System.out.println(Arrays.toString(twoSumMethod2(num, 6)));
+
+    }
+    public static int[] twoSumMethod1(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if(i!=j && ((nums[i] + nums[j]) == target)) {
+                        return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+
+    public static int[] twoSumMethod2(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[]{numMap.get(complement), i};
+            }
+            numMap.put(nums[i], i);
+        }
+
+        return null;
+    }
+}
